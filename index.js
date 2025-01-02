f73466caaf2568daa245d40d9aaeda2008694b01
Prashanth <prashanthravikumar12@gmail.com>
2025-01-02 21:50:57 +0530

    Added POST Method

--- index.js
+++ index.js
@@ -1,18 +1,33 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const app = express()
+const express = require('express');
+const mongoose = require('mongoose');
+const Product = require('./models/product.model.js');
+const app = express();
 
-app.listen(3000, () => {
-    console.log("Port is running at 3000");
+app.use(express.json());
+
+app.get('/', (req,res)=>{
+    res.send("Hello from Node API");
 });
 
-app.get('/', (req, res) => {
-    res.send("Hello from Node API Server");
+app.post('/api/products', async (req,res) => {
+   
+    try{
+        const product = await Product.create(req.body);
+        res.status(200).json(product);
+        }
+    catch(error) {
+        res.status(400).json({message: error.message});
+    }
+       
 });
+
 
 mongoose.connect('mongodb://127.0.0.1:27017/test')
 .then(() => {
     console.log('Connected!');
+    app.listen(3000, () => {
+        console.log("Port is running at 3000");
+    });
 })
 .catch(() =>{
     console.log("connection failed!")
